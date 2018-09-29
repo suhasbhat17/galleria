7f6d4324220d6b80ea5d07f308d44b1e5e59626a
Suhas Srinivasa Bhat <sbhat1@hawk.iit.edu>
2018-09-29 18:13:01 -0500

    intial commit

---
+++ src/main/java/com/suhas/galleria/galleria_app/GalleriaAppApplication.java
@@ -0,0 +1,12 @@
+package com.suhas.galleria.galleria_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalleriaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GalleriaAppApplication.class, args);
+	}
+}
