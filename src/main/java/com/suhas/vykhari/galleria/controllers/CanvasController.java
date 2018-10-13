461492a7e71e824e3716958a71b5fec9bb866760
Suhas Srinivasa Bhat <sbhat1@hawk.iit.edu>
2018-10-13 14:11:52 -0500

    added index.html

--- src/main/java/com/suhas/vykhari/galleria/controllers/CanvasController.java
+++ src/main/java/com/suhas/vykhari/galleria/controllers/CanvasController.java
@@ -3,6 +3,8 @@ package com.suhas.vykhari.galleria.controllers;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.suhas.vykhari.galleria.models.Canvas;
@@ -10,7 +12,13 @@ import com.suhas.vykhari.galleria.models.Canvas;
 @RestController
 public class CanvasController {
 	
-	public List<Canvas> getCanvas(){
+	@RequestMapping("/canvas")
+	public List<Canvas> getAllCanvas(){
 		return new ArrayList<>();
+	}
+	
+	@RequestMapping("/canvas/{id}")
+	public Canvas getAllCanvas(@PathVariable String id){
+		return new Canvas();
 	}
 }
--- src/main/java/com/suhas/vykhari/galleria/controllers/HomeController.java
+++ src/main/java/com/suhas/vykhari/galleria/controllers/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 
 	@RequestMapping("/")
-	public String welcome() {
+	public String index() {
 		return "index";
 	}
 }
